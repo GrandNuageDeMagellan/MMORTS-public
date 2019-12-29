8568a6074828c622a2ae687ccb1ad6c984592f4c
GLAZ\Glaz <steven59655@hotmail.fr>
2019-12-29 02:34:38 +0100

    Test 2 CHANGELOG.php

--- XG-Proyect-v4 UNOFFICIAL/src/upload/application/language/french/CHANGELOG.php
+++ XG-Proyect-v4 UNOFFICIAL/src/upload/application/language/french/CHANGELOG.php
@@ -5,13 +5,13 @@ $lang['changelog'] = array(
 // AN ERROR EDITING THIS FILE WILL RETURN A PHP ERROR IN YOUR CHANGELOG PAGE
 // EDIT FROM HERE [FROM THE FOLLOWING LINE REPLICATE]
     '3.2.0' => ' 08/11/2013
-- Ejemplo 3.-
+- Exemple 1.-
 ',
     '3.1.0' => ' 13/06/2013
-- Ejemplo 2.-
+- Exemple 2.-
 ',
     '3.0.0' => ' 13/05/2013
-- Ejemplo 1.-
+- Exemple 3.-
 ',
 // TO HERE [UNTIL THE PREVIOUS LINE]
 // AN ERROR EDITING THIS FILE WILL RETURN A PHP ERROR IN YOUR CHANGELOG PAGE
